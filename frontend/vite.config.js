e9cf9412e47a344f10adc1262b501eada8966712
peynadol <peynadol@gmail.com>
2025-04-21 11:23:00 +0100

    edit vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: "/",
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  build: {
-    outDir: "dist",
-  },
+    build: {
+      outDir: "dist",
+    },
+  };
 });
